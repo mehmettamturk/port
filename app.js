9ec190f9ac6ea6317773895b91fb7ef1022d79b3
Mehmet Tamtürk <mehmettamturk@tart.com.tr>
2013-11-07 23:33:45 +0200

    Code clean.

--- app.js
+++ app.js
@@ -75,5 +75,5 @@ var kit = function(val) {
 }
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Server Start Port:' + app.get('port'));
 });
